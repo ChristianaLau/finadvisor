6cfb2c91a1a34d2bbe2cc43de41fc87481adf961
codyub <codylchan@gmail.com>
2025-05-03 01:49:30 -0400

    some peseudo code for what we need for advice page

---
+++ app/advice/page.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+
+// goal is to grow saving
+//   put it into growth savings account
+//   invest
+//     show options for investing
+// if goal is to save more
+//   stop spending if spending too much
+//     they would need to categorize spending to see where they can save
+//     divide spending by categories, 
+//       needs vs wants
+//         needs = bills, food
+//         priority list for wants ranking 1-5
+//         wants = boba, vacation
+
+
+const advicePage = () => {
+  const [finData,setFinData] = useState()
+  // fin data should be in weekly, biweekly or monthly view based on settings
+  const [timeperiod,settimePeriod] = useState()
+  useEffect(()=>{
+    // get fin data if stored
+    // update db data upon unmount with saved fin data
+  })
+  const updateSpendingData=()=>{
+    // update statedata
+  }
+
+  const saveSpendingData=()=>{
+    // update db with spending data
+  }
+  const deleteSpendingData=()=>{}
+  return (
+    <div className="surveypage">
+        {/* display pie chart if data */}
+        {/* display what they can save on */}
+    </div>
+  );
+};
+export default advicePage;
