e915b48e860f5b388efce58e1f9f03e8f49da90d
ChristianaLau <clau.comp8@gmail.com>
2025-05-03 01:01:45 -0400

    set up webhook

--- middleware.js
+++ middleware.js
@@ -1,9 +1,18 @@
-import { authMiddleware } from '@clerk/nextjs';
+import { clerkMiddleware } from '@clerk/nextjs/server';
 
-export default authMiddleware({
-  publicRoutes: ['/'],
+export default clerkMiddleware({
+  publicRoutes: [
+    '/',              
+    '/about',            
+    '/api/webhook',      
+  ],
 });
 
 export const config = {
-  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: [
+    // Skip Next.js internals and static files
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
 };
