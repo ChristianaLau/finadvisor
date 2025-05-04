223c90e54826c9ac9a2348555f3ef9bf3bfbbf16
codyub <codylchan@gmail.com>
2025-05-03 20:56:26 -0400

    survey update for income

--- app/advice/InitialSurvey.jsx
+++ app/advice/InitialSurvey.jsx
@@ -1,7 +1,8 @@
 "use client";
 import { createFinDataProfile } from "@/lib/actions/finData.actions";
-import { periodTypes, periodDefault } from "@/app/constants";
+import { periodTypes, incomePeriodDefault } from "@/app/constants";
 import { useState, useEffect } from "react";
+import { match } from "assert";
 export default function InitialSurvey({ jobs = [] }) {
   const updateSpendingData = () => {
     // update statedata
@@ -13,22 +14,60 @@ export default function InitialSurvey({ jobs = [] }) {
     rent: 0,
     unemployed: false,
     jobName: "",
-    payPeriod: 0,
+    payPeriod: incomePeriodDefault,
+    amountperPayPeriod: 0,
+    amountperHour: 0,
     hoursPerPeriod: 0,
     startDate: new Date().toISOString().slice(0, 10),
-    endDate:"",
+    endDate: "",
     recurringEnd: "",
   });
   const handleChange = (e) => {
     const { name, value, type, checked } = e.target;
     // console.log(name, value, type, checked)
-    setForm((prev) => ({
-      ...prev,
-      [name]: type === "checkbox" ? checked : value,
-    }));
+    console.log(name, value, type, checked);
+    setForm((prev) => {
+      const newData = {
+        ...prev,
+        [name]:
+          type === "checkbox"
+            ? checked
+            : type === "number"
+            ? Number(value)
+            : value,
+      };
+      if(name ==="hoursPerPeriod" || name ==="amountperHour" ){
+        if (Number(newData.amountperHour)>0 && Number(newData.hoursPerPeriod)>0){
+          newData.amountperPayPeriod = (Number(newData.amountperHour)*100*Number(newData.hoursPerPeriod))/100
+        }
+      }
+      if(name ==="amountperPayPeriod" ){
+        if ( Number(newData.hoursPerPeriod)>0){
+          newData.amountperHour = ((Number(newData.amountperPayPeriod)*100/Number(newData.hoursPerPeriod))/100).toFixed(2)
+        }
+      }
+      switch (
+        name
+        // case "hoursPerPeriod":
+        //   if (newData.amountperHour && Number(newData.hoursPerPeriod)>0){
+        //     newData.amountperPayPeriod=newData.amountperHour*newData.amountperHour
+        //   }
+        // case "amountperHour":
+        //   if (newData.hoursPerPeriod){
+        //     newData.amountperPayPeriod=newData.amountperHour*newData.amountperHour
+        //   }
+        // case "amountperPayPeriod":
+        //   if (newData.hoursPerPeriod){
+        //     newData.amountperHour=(newData.amountperPayPeriod/newData.hoursPerPeriod).toFixed(2)
+        //   }
+      ) {
+      }
+      console.log(name, newData);
+      return newData;
+    });
   };
 
-  const handleSubmit =async (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
     const payload = {
@@ -37,14 +76,16 @@ export default function InitialSurvey({ jobs = [] }) {
       jobName: form.jobName,
       payPeriod: parseInt(form.payPeriod),
       hoursPerPeriod: form.hoursPerPeriod,
+      amountperPayPeriod: form.amountperPayPeriod,
+      amountperHour: form.amountperHour,
       startDate: form.startDate,
       recurringEnd: "",
-      endDate:form.startDate,
+      endDate: form.startDate,
       updated: new Date().toISOString(),
     };
     console.log("Submitted data:", payload);
     let added = await createFinDataProfile(payload);
-    console.log(added)
+    console.log(added);
     // Send to API or save to state/storage
   };
   return (
@@ -91,18 +132,40 @@ export default function InitialSurvey({ jobs = [] }) {
                 ))}
               </select>
             </div>
-            {form.payPeriod  > 1 && (
-              <div className="form-set">
-                <label>Hours per pay Period</label>
-                <input
-                  type="number"
-                  name="hoursPerPeriod"
-                  value={form.hoursPerPeriod}
-                  onChange={handleChange}
-                  className="adv-input"
-                />
-              </div>
+            {form.payPeriod > 1 && (
+              <>
+                <div className="form-set">
+                  <label>hourly wage</label>
+                  <input
+                    type="currency"
+                    name="amountperHour"
+                    value={form.amountperHour}
+                    onChange={handleChange}
+                    className="adv-input"
+                  />
+                </div>
+                <div className="form-set">
+                  <label>Hours per pay Period</label>
+                  <input
+                    type="number"
+                    name="hoursPerPeriod"
+                    value={form.hoursPerPeriod}
+                    onChange={handleChange}
+                    className="adv-input"
+                  />
+                </div>
+              </>
             )}
+            <div className="form-set">
+              <label>Amount Earned per Pay period</label>
+              <input
+                type="currency"
+                name="amountperPayPeriod"
+                value={form.amountperPayPeriod}
+                onChange={handleChange}
+                className="adv-input"
+              />
+            </div>
             <div className="form-set">
               <label>Date Start</label>
               <input
--- app/constants.jsx
+++ app/constants.jsx
@@ -7,10 +7,11 @@ export const periodTypes = [
   { label: "Monthly", value: 30 },
   { label: "yearly", value: 365 },
 ];
+export const incomePeriodDefault = 14;
 
 export const IncomePeriodTypes = [
   { label: "hourly", value: 0 },
-  { label: "One Time", value: periodDefault },
+  { label: "One Time", value: incomePeriodDefault },
   { label: "Weekly", value: 7 },
   { label: "Biweekly", value: 14 },
   { label: "Monthly", value: 30 },
